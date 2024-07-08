4331d096b05dd78a0f88fdb94cc6eac1221ea18f
davevad93 <davide.presti@iesdoctorbalmis.com>
2024-07-09 00:44:51 +0200

    Update PasswordGenerator.js adding new features

--- my-app/src/components/PasswordGenerator.js
+++ my-app/src/components/PasswordGenerator.js
@@ -3,9 +3,15 @@ import './PasswordGenerator.css';
 
 const PasswordGenerator = () => {
     const [password, setPassword] = useState('');
+    const [length, setLength] = useState(12);
+    const [useUpper, setUseUpper] = useState(true);
+    const [useLower, setUseLower] = useState(true);
+    const [useDigits, setUseDigits] = useState(true);
+    const [useSpecial, setUseSpecial] = useState(true);
+    const [excludeDuplicates, setExcludeDuplicates] = useState(true);	
 
     const generatePassword = async () => {
-        const response = await fetch('http://localhost:5000/generate-password');
+        const response = await fetch(`http://localhost:5000/generate-password?length=${length}&use_upper=${useUpper}&use_lower=${useLower}&use_digits=${useDigits}&use_special=${useSpecial}&exclude_duplicates=${excludeDuplicates}`);
         const data = await response.json();
         setPassword(data.password);
     };
@@ -21,10 +27,59 @@ const PasswordGenerator = () => {
             <button onClick={generatePassword}>Generate Password</button>
             {password && (
                 <div>
-                    <p>{password}</p>
+                    <input type="text" value={password} readOnly />
                     <button onClick={copyToClipboard}>Copy to Clipboard</button>
                 </div>
             )}
+            <div>
+                <label>Password length:</label>
+                <input
+                    type="range"
+                    min="4"
+                    max="32"
+                    value={length}
+                    onChange={(e) => setLength(e.target.value)}
+                />
+                <span>{length}</span>
+            </div>
+            <div>
+                <label>Characters used:</label>
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={useUpper}
+                        onChange={(e) => setUseUpper(e.target.checked)}
+                    /> ABC
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={useLower}
+                        onChange={(e) => setUseLower(e.target.checked)}
+                    /> abc
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={useDigits}
+                        onChange={(e) => setUseDigits(e.target.checked)}
+                    /> 123
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={useSpecial}
+                        onChange={(e) => setUseSpecial(e.target.checked)}
+                    /> #$&
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={excludeDuplicates}
+                        onChange={(e) => setExcludeDuplicates(e.target.checked)}
+                    /> Exclude Duplicate Characters
+                </label>
+            </div>			
         </div>
     );
 };
