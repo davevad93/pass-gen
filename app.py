174ee43ee286a06ca3a2ff404a01f47ddd812a0c
davevad93 <davide.presti@iesdoctorbalmis.com>
2024-07-10 00:19:03 +0200

    Update app.py (minor fixes)

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import string
 app = Flask(__name__)
 CORS(app)
 
-def generate_password(length=12, use_upper=True, use_lower=True, use_digits=True, use_special=True, exclude_duplicates=False):
+def generate_password(length=12, use_upper=True, use_lower=True, use_digits=True, use_special=False, exclude_duplicates=False):
     characters = ''
     if use_upper:
         characters += string.ascii_uppercase
@@ -30,8 +30,8 @@ def get_password():
     use_upper = request.args.get('use_upper', 'true').lower() == 'true'
     use_lower = request.args.get('use_lower', 'true').lower() == 'true'
     use_digits = request.args.get('use_digits', 'true').lower() == 'true'
-    use_special = request.args.get('use_special', 'true').lower() == 'true'
-    exclude_duplicates = request.args.get('exclude_duplicates', 'true').lower() == 'true'
+    use_special = request.args.get('use_special', 'false').lower() == 'true'
+    exclude_duplicates = request.args.get('exclude_duplicates', 'false').lower() == 'true'
     
     password = generate_password(length, use_upper, use_lower, use_digits, use_special, exclude_duplicates)
     return jsonify(password=password)
