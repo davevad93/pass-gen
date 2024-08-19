9001017c433ec4b1774fd1410c1e090ae7ded3cc
davevad93 <94308234+davevad93@users.noreply.github.com>
2024-08-19 17:20:14 +0200

    Add i18n index.js

---
+++ my-app/src/i18n/index.js
@@ -0,0 +1,15 @@
+import en from './locales/en';
+import es from './locales/es';
+import it from './locales/it';
+
+export const resources = {
+    en: {
+        translation: en
+    },
+    es: {
+        translation: es
+    },
+    it: {
+        translation: it
+    }
+};
